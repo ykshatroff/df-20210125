75788b272fb57e2cacfa2a4308de463346cb3f8c
Yuriy Shatrov <yks-uno@ya.ru>
2021-02-17 10:38:03 +0200

    Added pytest class/exception

--- df/hello.py
+++ df/hello.py
@@ -5,5 +5,24 @@ def hello():
     return f"Hello world, {os.environ['SECRET']}"
 
 
+class User:
+    def __init__(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
+
+class Hello:
+    def __init__(self):
+        self.user = User(os.environ['SECRET'])
+
+    def get_user(self):
+        return self.user
+
+    def output(self):
+        return f"Hello world, {self.get_user().name}"
+
+
 if __name__ == '__main__':
     print(hello())
--- df/tests/test_hello.py
+++ df/tests/test_hello.py
@@ -1,26 +1,48 @@
 import os
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 
 import pytest
 
-from hello import hello
-
-
-@pytest.fixture()
-def os_environ_user():
-    user = 'user'
-    with patch.object(os, 'environ', {'SECRET': user}):
-        yield user
-    # wrap up
+from hello import hello, Hello, User
 
 
 def test_hello():
     assert hello() == "Hello world, asdf"
 
 
-def test_hello_user(os_environ_user):
-    assert hello() == f"Hello world, {os_environ_user}"
+@patch.object(os, 'environ')
+def test_hello_user(mock):
+    assert hello() == f"Hello world, user"
 
 
 def test_hello_bad():
     assert hello() == "Hello world, asdf"
+
+
+def test_mock():
+    mock = Mock()
+    assert isinstance(mock.attribute, Mock)
+    assert isinstance(mock.attribute.subattribute, Mock)
+    assert isinstance(mock.method().subattribute, Mock)
+    assert isinstance(mock.method().subattribute(), Mock)
+
+
+def test_class_hello():
+    assert User("user").get_name() == 'user'
+    with patch.object(os, 'environ', {"SECRET": 'user'}):
+        assert Hello().output() == f"Hello world, user"
+
+    hello_object = Hello()
+    with patch.object(hello_object, 'get_user') as mock_user:
+        mock_user.return_value = User("NOT USER")
+        assert hello_object.output() == f"Hello world, NOT USER"
+
+    with patch.object(hello_object, 'get_user') as mock_user:
+        mock_user.side_effect = ValueError("Bad user")
+
+        with pytest.raises(ValueError) as error:
+            hello_object.output()
+
+        assert error.match("Bad user")
+        assert isinstance(error.value, ValueError)
+
